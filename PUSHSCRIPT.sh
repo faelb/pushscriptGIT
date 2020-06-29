1233bb7a7b9a1e9c1b10c9779d7e27c8c8e20ff5
Christoph Felber <49062695+faelb@users.noreply.github.com>
2020-06-29 10:47:59 +0200

    updated script with some comments

--- PUSHSCRIPT.sh
+++ PUSHSCRIPT.sh
@@ -1,3 +1,6 @@
+#used to push already existing projects on Desktop to a created empty GIT URL
+#übergeben wird die Adresse inklusive .git    f.e. https://github.com/faelb/piProject.git
+
 read -p "Give me the URL" URL
 git init
 git add .
@@ -5,3 +8,4 @@ git commit -m "initial commit from Windows"
 git remote add origin $URL
 git push -u origin master
 
+read -p "finished" NONCE
